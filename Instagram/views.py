2069fe6ea20e4a0c19a634aef5e2080106663514
Zeeshan PC <zeeshan.shafeek@gmail.com>
2023-04-07 14:05:39 +0500

    initial working

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
+import requests
 
 access_token = "this didnt work"
 # Create your views here.
 def home(request):
 
     id = 210007288341422
-    permissions = "pages_show_list%2Cinstagram_basic%2Cinstagram_manage_comments%2Cinstagram_manage_insights%2Cinstagram_content_publish%2Cinstagram_manage_messages%2Cpages_read_engagement%2Cpages_manage_metadata%2Cpublic_profile"
     redirect_uri = "https://127.0.0.1:8000%2Fapp%2Ftoken"
-    url = f"https://www.facebook.com/v16.0/dialog/oauth?response_type=code&display=page&client_id={id}&redirect_uri={redirect_uri}&auth_type=rerequest&scope={permissions}"
-
+    client_id = 9023666841038430
+    config_id = 172127899060667
+    url = f'https://www.facebook.com/v16.0/dialog/oauth?client_id={client_id}&redirect_uri={redirect_uri}&state="%7Bst=state123abc,ds=123456789%7D"&config_id={config_id}'
     print(access_token)
     context = {
         'url': url
@@ -17,10 +18,22 @@ def home(request):
 
 
 def token(request):
-    access_token = request.GET.get('access_token', '')
+    
+    code = request.GET.get('code', '')
     context = {
-        "access_token" : access_token
+        "code" : code
     }
-    return render(request, 'token.html', context)
+    params = {
+            'client_id': '139791198861160',
+            'client_secret': 'bd6198dcc60a6a43ad94ebb7f6e9b4c8',
+            'code': code,
+            'redirect_uri': '<your_redirect_uri>'
+        }
+    response = requests.get('https://graph.facebook.com/v16.0/oauth/access_token', params=params)
+    if response.status_code == 200:
+        access_token = response.json()['access_token']
+        # do something with the access_token
+    else:
+        # handle the error
 
 
